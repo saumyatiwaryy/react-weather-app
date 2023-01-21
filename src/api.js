d63b14899d6c31e2245dfb6d2e896252e3ec3f20
Saumya Tiwari <73695360+saumyatiwaryy@users.noreply.github.com>
2023-01-21 22:17:11 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "c4736afabemsha911bd069773f9bp1534c2jsne344d3cc9090",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "c1de2c89ed7e3d0ffd7789b1349d4cf1"; // enter your key from openweather API
